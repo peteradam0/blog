7107deee32ede269a70d5dc1028523a5a4ef5bfd
peteradam0 <adampeter1998@yahoo.com>
2025-04-07 11:58:47 +0300

    feat: add header icon

---
+++ src/components/icons/footprint.tsx
@@ -0,0 +1,9 @@
+export default function FootprintIcon() {
+  return (
+    <img
+      className="w-10 h-10 inline-block transition-transform grop-hover:rotate-[20deg]"
+      src="../../../public/footprint.svg"
+      alt="logo"
+    ></img>
+  )
+}
